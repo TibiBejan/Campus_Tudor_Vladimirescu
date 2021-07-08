acd895ee648a7fd05669a57c6c2109719ee4bdd8
TibiBejan <tiberiu.bejan98@gmail.com>
2021-07-08 09:40:45 +0300

    Cors - updated cookie

--- server/utils/token.js
+++ server/utils/token.js
@@ -13,10 +13,10 @@ const createToken = (user, statusCode, message, res) => {
 
     // GENERATE HTTP_ONLY COOCKIE FOR CLIENT SIDE
     res.cookie('jwt', token, {
+        httpOnly: true,
         expires: new Date(Date.now() + process.env.JWT_EXPIRES_COOKIE_IN * 24 * 60 * 60 * 1000), // MS
         secure: process.env.NODE_ENV === 'production' ? true : false,
-        httpOnly: true,
-        domain: 'campus-tudor-vladimirescu.netlify.app',
+        sameSite:'none',
     });
 
     return res.status(statusCode).json({
